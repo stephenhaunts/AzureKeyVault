e51b3378ffa98aa1d53d63bf50b2539b3dc63dd6
stephen haunts <stephen.haunts@outlook.com>
2018-04-29 21:30:17 +0100

    Add code for local key wrapping demonstration.

---
+++ KeyWrapping/AesEncryption.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace AzureKeyVault.KeyWrapping
+{
+    public class AesEncryption
+    {
+        public static byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    var decryptBytes = memoryStream.ToArray();
+
+                    return decryptBytes;
+                }
+            }
+        }
+    } 
+}
---
+++ KeyWrapping/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureKeyVault.KeyWrapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyVault().GetAwaiter().GetResult();
+        }
+
+        public static async Task KeyVault()
+        {
+            IKeyVault vault = new KeyVault();
+
+            const string MY_KEY_NAME = "StephenHauntsKey";
+            string keyId = await vault.CreateKeyAsync(MY_KEY_NAME);
+
+            byte[] localKey = Random.GenerateRandomNumber(32);
+
+            // Encrypt our local key with Key Vault and Store it in the database
+            byte[] encryptedKey = await vault.EncryptAsync(keyId, localKey);
+
+
+            // Get our encrypted key from the database and decrypt it with the Key Vault.
+            byte[] decryptedKey = await vault.DecryptAsync(keyId, encryptedKey);
+
+            // Now we have recovered the key with the Key Vault we can encrypt with AES locally.
+            byte[] iv = Random.GenerateRandomNumber(16);
+            byte[] encryptedData = AesEncryption.Encrypt(Encoding.ASCII.GetBytes("MEGA TOP SECRET STUFF"), decryptedKey, iv);
+            byte[] decryptedMessage = AesEncryption.Decrypt(encryptedData, decryptedKey, iv);
+
+            var encryptedText = Convert.ToBase64String(encryptedData);
+            var decryptedData = Encoding.UTF8.GetString(decryptedMessage);
+
+            // Remove HSM backed key
+            await vault.DeleteKeyAsync(MY_KEY_NAME);
+            Console.WriteLine("Key Deleted : " + keyId);
+
+        }
+    }
+}
